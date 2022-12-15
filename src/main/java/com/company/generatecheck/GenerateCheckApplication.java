d8315bb7d01a5302d26577912623b8959c973751
AlekseiKasperovich <AleksKasperovich@yandex.by>
2022-12-15 13:36:09 +0300

    first commit

---
+++ src/main/java/com/company/generatecheck/GenerateCheckApplication.java
@@ -0,0 +1,13 @@
+package com.company.generatecheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerateCheckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenerateCheckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/company/generatecheck/GenerateCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.generatecheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenerateCheckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
